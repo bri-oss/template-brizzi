1961fdda93548fc2388d03e326a5bbdc46753c29
AlfaRiza <malfariza45@gmail.com>
2024-07-15 09:48:12 +0700

    chore: template brizzi

---
+++ src/check_topup_status.php
@@ -0,0 +1,43 @@
+<?php
+
+
+require __DIR__ . '/../vendor/autoload.php';
+
+Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..' . '')->load();
+
+require __DIR__ . '/../../briapi-sdk/autoload.php';
+
+use BRI\Brizzi\Brizzi;
+use BRI\Util\GetAccessToken;
+
+$clientId = $_ENV['CONSUMER_KEY']; // customer key
+$clientSecret = $_ENV['CONSUMER_SECRET']; // customer secret
+$pKeyId = $_ENV['PRIVATE_KEY']; // private key
+
+// url path values
+$baseUrl = 'https://sandbox.partner.api.bri.co.id'; //base url
+
+// change variables accordingly
+$partnerId = 'feedloop'; //partner id
+$channelId = '12345'; // channel id
+
+$getAccessToken = new GetAccessToken();
+
+[$accessToken, $timestamp] = $getAccessToken->get(
+  $clientId,
+  $pKeyId,
+  $baseUrl
+);
+
+$directDebit = new Brizzi();
+
+$response = $directDebit->checkTopupStatus(
+  $clientSecret = $clientSecret, 
+  $partnerId = $partnerId,
+  $baseUrl,
+  $accessToken, 
+  $channelId,
+  $timestamp
+);
+
+echo $response;
